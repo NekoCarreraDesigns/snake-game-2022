79b6f93a17e8d7f8e55b1d7af1faecfcf7f6ee90
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-04 14:27:38 -0700

    added logic to random snack funtion

--- main.py
+++ main.py
@@ -129,8 +129,19 @@ def redrawWindow(surface):
     pygame.display.update()
 
 
-def randomSnack(rows, items):
-    pass
+def randomSnack(item):
+    global rows
+    positions = item.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+
+    return (x, y)
 
 
 def message_box(subject, content):
@@ -143,6 +154,7 @@ def main():
     rows = 20
     win = pygame.display.set_mode((width, width))
     s = snake((235, 153, 23), (10, 10))
+    snack = cube(randomSnack(rows, s), color=(0, 255, 0))
     flag = True
 
     clock = pygame.time.Clock()
@@ -151,6 +163,9 @@ def main():
         pygame.time.delay(50)
         clock.tick(10)
         s.move()
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color=(0, 255, 0))
         redrawWindow(win)
     pass
 
